662493dddd765cb2a25d840d91ef5a1bac3303ea
paolaqv <paola.quispe.v@ucb.edu.bo>
2024-10-20 05:33:30 -0400

    agregando configuracion

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,3 +1,4 @@
 from .mascota_service import MascotaService
 from .usuario_service import UsuarioService
+from .especie_service import EspecieService
 # Agrega más servicios según vayas creando más funcionalidades
