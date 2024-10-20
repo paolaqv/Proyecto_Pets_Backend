874cfbcac663195741f2ff75b142da61a212bd56
paolaqv <paola.quispe.v@ucb.edu.bo>
2024-10-20 05:31:51 -0400

    obtener especies

---
+++ app/routes/especie_routes.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, jsonify
+from app.services.especie_service import EspecieService
+
+especie_routes = Blueprint('especie_routes', __name__)
+
+@especie_routes.route('/get', methods=['GET'])
+def obtener_especies():
+    especies = EspecieService.obtener_todas_especies()
+
+    # Formatear las especies para enviarlas como respuesta JSON
+    especies_data = [
+        {
+            "id_especie": especie.id_especie,
+            "especie": especie.especie
+        }
+        for especie in especies
+    ]
+    
+    return jsonify(especies_data), 200
