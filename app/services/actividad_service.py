42515a763c58bdea0cbc4ff734a9f87e31ffd11a
Rosario Calisaya Calderon <125493619+Kira2602@users.noreply.github.com>
2024-12-10 16:53:56 -0400

    Update actividad_service.py

--- app/services/actividad_service.py
+++ app/services/actividad_service.py
@@ -1,4 +1,8 @@
 from app.repositories.actividad_repository import ActividadRepository
+from datetime import datetime
+import pytz
+from app import db
+
 
 class ActividadService:
 
@@ -20,21 +24,43 @@ class ActividadService:
         return ActividadRepository.add_actividad(data)
     
     
+    @staticmethod
     def obtener_todas1():
-        # Obtener las actividades desde el repositorio
+        zona_horaria = pytz.timezone('America/La_Paz')
         actividades = ActividadRepository.get_todas1()
 
-        # Procesar datos para incluir nombres
         actividades_data = []
+        now = datetime.now(zona_horaria)  # Hora actual
+
         for actividad in actividades:
+            # Omitir actividades canceladas
+            if actividad.estado == "Cancelado":
+                continue
+
+            fecha_hora = actividad.fecha_hora.astimezone(zona_horaria)
+
+            # Procesa los datos
             actividades_data.append({
                 "id_actividad": actividad.id_actividad,
-                "fecha_hora": actividad.fecha_hora,
+                "fecha_hora": fecha_hora.isoformat(),
                 "descripcion": actividad.descripcion,
-                "mascota_nombre": actividad.mascota.nombre,  # Nombre de la mascota
-                "tipo_actividad_nombre": actividad.tipo_actividad.tipo  # Nombre del tipo de actividad
+                "mascota": actividad.mascota.nombre if actividad.mascota else "Desconocida",
+                "tipo_actividad": actividad.tipo_actividad.tipo if actividad.tipo_actividad else "Sin categoría",
+                "estado": actividad.estado  # Incluye el estado
             })
 
         return actividades_data
+
+
+    @staticmethod
+    def cancelar_actividad(actividad_id):
+        actividad = ActividadRepository.get_actividad_by_id(actividad_id)
+        if actividad:
+            actividad.estado = "Cancelado"  # Cambia el estado a "Cancelado"
+            db.session.commit()  # Guarda los cambios en la base de datos
+            return actividad
+        return None
+
+
 
 
